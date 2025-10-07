8d3a53c7535dc487ecc9542d91b57c9a7f247128
LucasGrem <lucasgrem07@gmail.com>
2025-10-07 19:01:09 -0300

    feat: finalizei o projeto

--- src/main/java/com/senai/escola/Interface/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.senai.escola.Interface;
-
-import com.senai.escola.Models.Aluno;
-import com.senai.escola.Models.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
